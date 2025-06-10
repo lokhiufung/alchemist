1381369ea3a1ee632d8f28ab283a4dafc7f2c311
lokhiufung <lokhiufung123@gmail.com>
2025-06-10 11:54:34 -0400

    refactor: enhance FutureProduct constructor with type hints and docstring

--- alchemist/products/future_product.py
+++ alchemist/products/future_product.py
@@ -3,9 +3,17 @@ from alchemist.products.base_product import BaseProduct
 class FutureProduct(BaseProduct):
    product_type = 'FUTURE'
    
-   def __init__(self, name, base_currency, exch, contract_month):
-        super().__init__(name, base_currency, exch)
-        self.contract_month = contract_month
+   def __init__(self, name: str, base_currency: str, exch: str, contract_month: str):
+      """Product class for futures
+
+      Args:
+         name (str): _description_
+         base_currency (str): _description_
+         exch (str): _description_
+         contract_month (str): e.g `2025-06` for June 2025 contract 
+      """
+      super().__init__(name, base_currency, exch)
+      self.contract_month = contract_month
 
 
 
