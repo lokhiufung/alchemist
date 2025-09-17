f772b6a49c34935227ae0fddbb5fa70d4b27844c
lokhiufung <lokhiufung123@gmail.com>
2025-09-16 23:02:46 -0400

    feat: add FutureContractCommission class with margin and multiplier attributes

--- alchemist/commission.py
+++ alchemist/commission.py
@@ -4,7 +4,13 @@ from dataclasses import dataclass
 @dataclass
 class Commission:
     commssion: float
+    is_percentage: bool = False
     
 
 
-
+@dataclass
+class FutureContractCommission(Commission):
+    initial_margin: float = 0.0
+    maintainance_margin: float = 0.0
+    multiplier: float = 1.0
+    
